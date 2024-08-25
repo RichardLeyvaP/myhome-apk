334181744018aaefb4e22e588b87d65b2240e0ec
RichardLeyvaP <richardleyvap1991@gmail.com>
2024-08-25 09:54:13 -0300

    controlando los errores desde el main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: unused_element, depend_on_referenced_packages
 
-import 'package:firebase_core/firebase_core.dart';
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:myhome/app_initializer.dart';
 import 'package:myhome/dependency_injection.dart';
@@ -18,17 +20,33 @@ void main() async {
   //initializeDateFormatting('es_ES', null).then((_) {
   initializeDateFormatting('pt', null).then((_) async {
     WidgetsFlutterBinding.ensureInitialized();
+
+    runZonedGuarded(() {
+      //controlar errores
+      FlutterError.onError = (details) {
+        print('Controlando errores flutter');
+        Zone.current.handleUncaughtError(details.exception, details.stack ?? StackTrace.empty);
+      };
+      runApp(
+        MultiProvider(
+          providers: providers, //esto es para la autenticacion por la huella dactilar
+          child: const BlocsProviders(),
+        ),
+      );
+    }, (error, stack) {
+      if (kDebugMode) {
+        print('Controlando errores flutter-ZONA');
+        // log(error.toString(), stackTrace: stack);
+      } else {
+        //aqui mandar al backend los errores
+        print('Controlando errores flutter-ZONA-SIN ESTAR EN DEBUGER');
+      }
+    });
     // Cargar las traducciones iniciales (en este caso, inglés)
     await TranslationManager.loadDefaultTranslations();
     // Cargar las traducciones antes de iniciar la aplicación
     //await _requestPermissions();
     // await Firebase.initializeApp();
-    runApp(
-      MultiProvider(
-        providers: providers, //esto es para la autenticacion por la huella dactilar
-        child: const BlocsProviders(),
-      ),
-    );
   });
 }
 
